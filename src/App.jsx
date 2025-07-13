7c92511f2735af7553f46d905b885c4b36b55791
saadrasheed82 <saadrasheed828@gmail.com>
2025-07-13 16:40:22 +0500

    13/7/25

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import Testimonials from "./sections/Testimonials";
 import Footer from "./sections/Footer";
 import Contact from "./sections/Contact";
-import TechStack from "./sections/TechStack";
 import Experience from "./sections/Experience";
 import Hero from "./sections/Hero";
 import ShowcaseSection from "./sections/ShowcaseSection";
